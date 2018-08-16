b4244865ebdcadcad94d3de64b375d1c7f8f5327
iiitoo <zy@iiitoo.com>
2018-08-16 22:15:29 +0800

    第一课

---
+++ NO.1/index.js
@@ -0,0 +1,19 @@
+var getHelloTo;
+var fullMessage;
+
+
+function getHelloTo(name) {
+    console.log(`Hello to ${name}`);
+};
+
+getHelloTo("Kandra");
+
+
+var add;
+add = function (number1, number2) {
+    var total = number1 + number2;
+    return total;
+};
+
+var sum = add(50, 23);
+console.log(sum);

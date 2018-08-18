af79ccc128b65af54494addca19aacfb45800312
iiitoo <zy@iiitoo.com>
2018-08-18 19:35:22 +0800

    第五课补充

--- No.5/index.js
+++ No.5/index.js
@@ -11,8 +11,15 @@ function buildPlanet(name, position, type, radius, rank) {
         sizeRank: rank
     };
 };
-function (planet) {
+function getPlanetInfo(planet) {
     return planet.name.toUpperCase() + ": planet " + planet.position;
 };
 planet1 = buildPlanet("Jupiter", 5, "Gas Giant", 69911, 1);
-planet2 = buildPlanet()
+planet2 = buildPlanet("Neptune", 8, "Ice Giant", 24622, 4);
+
+
+console.log(planet1);
+
+
+console.log(getPlanetInfo(planet1));
+console.log(getPlanetInfo(planet2));
